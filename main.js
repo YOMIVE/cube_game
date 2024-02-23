62754780d48bf080106447633d37b0910852bd3a
YOMIVE <148207164+YOMIVE@users.noreply.github.com>
2024-02-23 19:59:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,163 @@
+var personId = 32;
+
+var Field_One_One = document.getElementById('11');
+var Field_One_Two = document.getElementById('12');
+var Field_One_Three = document.getElementById('13');
+var Field_Two_One = document.getElementById('21');
+var Field_Two_Two = document.getElementById('22');
+var Field_Two_Three = document.getElementById('23');
+var Field_Three_One = document.getElementById('31');
+var Field_Three_Two = document.getElementById('32');
+var Field_Three_Three = document.getElementById('33');
+
+
+
+function reloadGame(personId) {
+    switch(personId) {
+        case 11:
+            Field_One_One.style.cssText = "background-color: red;";
+            Field_One_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            break
+        case 12:
+            Field_One_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Two.style.cssText = "background-color: red;";
+            Field_One_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            break
+        case 13:
+            Field_One_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Three.style.cssText = "background-color: red;";
+            Field_Two_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            break
+        case 21: 
+            Field_One_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_One.style.cssText = "background-color: red;";
+            Field_Two_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            break
+        case 22:
+            Field_One_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Two.style.cssText = "background-color: red;";
+            Field_Two_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            break
+        case 23:
+            Field_One_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Three.style.cssText = "background-color: red;";
+            Field_Three_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            break
+        case 31: 
+            Field_One_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_One.style.cssText = "background-color: red;";
+            Field_Three_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            break
+        case 32:
+            Field_One_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Two.style.cssText = "background-color: red;";
+            Field_Three_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            break
+        case 33:
+            Field_One_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_One_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Two_Three.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_One.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Two.style.cssText = "background-color: rgb(100, 100, 100);";
+            Field_Three_Three.style.cssText = "background-color: red;";
+            break
+    }
+}
+
+reloadGame(personId);
+
+
+function btnTop() {
+    if(personId != 11 || personId != 12 || personId != 13) {
+        personId -= 10;
+        console.log("/\\")
+        console.log(personId)
+    }
+    reloadGame(personId);
+}
+
+function btnLeft() {
+    if(personId != 11 || personId != 21 || personId != 31) {
+        personId--;
+        console.log("<")
+        console.log(personId)
+    }
+    reloadGame(personId);
+}
+
+function btnRight() {
+    if(personId != 13 || personId != 23 || personId != 33) {
+        personId++;
+        console.log(">")
+        console.log(personId)
+    }
+    reloadGame(personId);
+}
+
+function btnBottom() {
+    if(personId != 31 || personId != 32 || personId != 33) {
+        personId += 10;
+        console.log("\\/")
+        console.log(personId)
+    }
+    reloadGame(personId);
+}
+
+
+
+
+
+
+
